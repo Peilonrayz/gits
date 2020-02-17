6d0a9387bc7eef5866ab2afec1d9fbb7ed350f8d
Peilonrayz <peilonrayz@gmail.com>
2020-02-17 16:07:09 +0000

    Add colour and remote information

--- src/gits/__main__.py
+++ src/gits/__main__.py
@@ -1,6 +1,31 @@
+import collections
 import subprocess
 import pathlib
 import os
+
+import colorama
+colorama.init()
+
+
+Status = collections.namedtuple('Status', 'code, commits, behind, ahead')
+
+
+def git_remote():
+    ret = subprocess.run(
+        ['git', 'config', '--get', 'remote.origin.url'],
+        capture_output=True,
+    )
+    return ret.stdout
+
+
+def git_status():
+    ret = subprocess.run(['git', 'status'], capture_output=True)
+    return Status(
+        ret.returncode,
+        b'nothing to commit, working tree clean' not in ret.stdout,
+        b'Your branch is behind of' in ret.stdout,
+        b'Your branch is ahead of' in ret.stdout,
+    )
 
 
 def find_uncommited_changes(root):
@@ -9,22 +34,28 @@ def find_uncommited_changes(root):
             if not path.is_dir():
                 continue
             os.chdir(path)
-            ret = subprocess.run(['git', 'status'], capture_output=True)
-            status = ' '
-            if ret.returncode:
-                status = '1'
-            elif b'nothing to commit, working tree clean' not in ret.stdout:
-                status = '!'
-            print(status, path.name)
-            # print(ret.returncode, path.name)
-            # print(ret.stdout.decode('utf-8'))
+            status = git_status()
+            color = colorama.Style.BRIGHT
+            if status.code:
+                color += colorama.Fore.WHITE
+            elif status.commits:
+                color += colorama.Fore.RED
+            else:
+                color = colorama.Style.DIM + colorama.Fore.CYAN
+
+            behind = '<-' if status.behind else '  '
+            remote = '^' if git_remote() else ' '
+            ahead = '->' if status.ahead else '  '
+            print(
+                f'{behind}{remote}{ahead}'
+                f' {color}{path.name}{colorama.Style.RESET_ALL}'
+            )
     finally:
         os.chdir(root)
 
 
 def main():
     find_uncommited_changes(pathlib.Path.cwd())
-        
 
 
 if __name__ == '__main__':
